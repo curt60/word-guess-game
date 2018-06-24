40a34d729b3f15b879cacb9f70ffff6d7252042d
Curtis Robinson <38477322+curt60@users.noreply.github.com>
2018-06-24 11:45:23 -0700

    updated win/loss screens and tweaked trigger logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 //declare global variables
-var words = ["cylon", "raider", "earth", "kobol"];
+// var words = ["cylon", "raider", "earth", "kobol", "toaster"];
+var words = ["cylon", "raider"];
 var wordIndex;
 var currentWord;
 var validInputRegEx = /^[a-z]$/i;
@@ -8,28 +9,54 @@ const MAX_GUESSES = 10;
 var guesses;
 var matchedLetters;
 var guessedLetters;
+var gameState = "new";
 
 //bind variables to HTML output elements
+var mainElement = document.querySelector("main");
 var winElement = document.querySelector(".wins");
 var wordElement = document.querySelector(".word");
 var guessesElement = document.querySelector(".guesses");
 var lettersElement = document.querySelector(".letters");
+var successElement = document.querySelector(".success-container");
+var failureElement = document.querySelector(".failure-container");
+var endWordElement = document.querySelectorAll(".end-word");
+var gameOverElement = document.querySelector(".game-over-container");
+var audioTheme = document.querySelector("audio.theme");
 
-//randomly select a word and reset variables
-function selectWord() {
+//initialize new round
+function initialize() {
+    //if all words played, end the game
+    if (words.length < 1) {
+        gameOverElement.style.opacity = 1;
+        gameState = "game-over";
+        return
+    }
+    //select random word and remove from list of possible words  
     wordIndex = Math.floor(Math.random() * words.length);
     currentWord = words.slice(wordIndex, wordIndex + 1)[0].toUpperCase();
     words.splice(wordIndex, wordIndex + 1);
+    //reset variables
     matchedLetters = [];
     guessedLetters = [];
     guesses = MAX_GUESSES;
-    displayGuesses();
+    gameState = "new";
+    //reset display elements
     displayWins();
+    displayGuesses();
+    displayMatchedLetters();
+    displayGuessedLetters();
+    mainElement.style.opacity = 1;
+    successElement.style.opacity = 0;
+    failureElement.style.opacity = 0;
+    endWordElement[0].innerHTML = currentWord;
+    endWordElement[1].innerHTML = currentWord;
+
+    //send current word to console (for testing)
     console.log(currentWord);
 }
 
 //return the current matched letters to display
-function matchedLetterDisplay() {
+function displayMatchedLetters() {
     var output = ""
     for (let i = 0; i < currentWord.length; i++) {
         if (matchedLetters[i]) {
@@ -39,18 +66,17 @@ function matchedLetterDisplay() {
             output += "_ ";
         }
     }
-    console.log("matched letters: " + output);
     wordElement.innerHTML = output;
     return output;
 }
 
 //return the guessed letters to display
-function guessedLetterDisplay() {
+function displayGuessedLetters() {
     var output = ""
     for (let i = 0; i < guessedLetters.length; i++) {
         output += guessedLetters[i] + " ";
     }
-    console.log("guessed letters: " + output);
+    lettersElement.innerHTML = output;
     return output;
 }
 
@@ -110,45 +136,62 @@ function checkMatch(letter) {
 
 //check for complete word matched
 function checkSuccess() {
-    var success = true;
+    var successFlag = true;
     for (let i = 0; i < currentWord.length; i++) {
-        if (!matchedLetters[i]) success = false;
+        if (!matchedLetters[i]) successFlag = false;
     }
-    return success;
+    return successFlag;
 }
 
+function successEvent() {
+    wins++;
+    displayWins();
+    mainElement.style.opacity = 0;
+    successElement.style.opacity = 1;
+    gameState = "end-round"
+}
 
-//select initial word and initialize variables
-selectWord();
-wordElement.innerHTML = matchedLetterDisplay();
+function failureEvent() {
+    mainElement.style.opacity = 0;
+    failureElement.style.opacity = 1;
+    gameState = "end-round"
+}
+
+//play theme music
+audioTheme.volume = 0.05;
+audioTheme.play();
+
+//initialize new round
+initialize();
 
 //listen for key input
 document.onkeyup = function() {
+    //check if end of round or end of game
+    if (gameState === "end-round") {
+        initialize();
+        return;
+    }
+    else if (gameState === "game-over") {
+        location.reload();
+    }
+
     var keyInput = event.key.toUpperCase();
 
     //check match status
     var matchStatus = checkMatch(keyInput);
     if (matchStatus === "match") {
-        wordElement.innerHTML = matchedLetterDisplay();
+        displayMatchedLetters();
         if (checkSuccess()) {
-            wins++;
-            displayWins();
-            // alert("You won this round!");
-            if (words.length < 1) alert("Game Over");
-            else selectWord();
+            successEvent();
         }
         else if (guesses < 1) {
-            alert("You lost this round");
-            if (words.length < 1) alert("Game Over");
-            else selectWord();
+            failureEvent();
         }
     }
     else if (matchStatus === "noMatch") {
-        lettersElement.innerHTML = guessedLetterDisplay();
+        displayGuessedLetters();
         if (guesses < 1) {
-            alert("You lost this round");
-            if (words.length < 1) alert("Game Over");
-            else selectWord();
+            failureEvent();
         }
     }
 }
